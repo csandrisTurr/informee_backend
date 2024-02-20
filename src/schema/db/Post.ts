ef680ff56a057768d4064657f0c8706e1e7cbb75
csandrisTurr <csaszarandras@turr.hu>
2024-02-20 08:41:18 +0100

    feat(P2-33): post schema

---
+++ src/schema/db/Post.ts
@@ -0,0 +1,28 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+
+@Schema()
+export class DbPost {
+  @Prop({ required: true })
+  id: string;
+  
+  // author of the post
+  @Prop({ required: true })
+  parent: string;
+
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ required: true })
+  content: string;
+
+  @Prop({ required: true })
+  creationDate: string;
+}
+
+export const DbPostSchema = SchemaFactory.createForClass(DbPost);
+export type DbPostDocument = HydratedDocument<DbPost>;
+export const DbPostName = 'post';
