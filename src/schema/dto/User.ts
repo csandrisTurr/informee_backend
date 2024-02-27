26fba338aaa2f8e795f9558b644c6f9e59a47e67
csandrisTurr <csaszarandras@turr.hu>
2024-02-27 08:26:11 +0100

    feat(P2-59): created user service

--- src/schema/dto/User.ts
+++ src/schema/dto/User.ts
@@ -1,28 +1,28 @@
-import { IsEmail, IsString, Length } from "class-validator";
+import { IsEmail, IsString, Length } from 'class-validator';
 
 export class RegisterUserDto {
-    @IsEmail()
-    @Length(0, 64)
-    email: string;
+  @IsEmail()
+  @Length(0, 64)
+  email: string;
 
-    @IsString()
-    @Length(3, 24)
-    username: string;
+  @IsString()
+  @Length(3, 24)
+  username: string;
 
-    @IsString()
-    @Length(3, 24)
-    displayName: string;
+  @IsString()
+  @Length(3, 24)
+  displayName: string;
 
-    // TODO: add check for sha512
-    @IsString()
-    password: string;
+  // TODO: add check for sha512
+  @IsString()
+  password: string;
 }
 
 export class LoginUserDto {
-    @IsEmail()
-    email: string;
+  @IsEmail()
+  email: string;
 
-    // TODO: add check for sha512
-    @IsString()
-    password: string;
+  // TODO: add check for sha512
+  @IsString()
+  password: string;
 }
