9e7cffc14a4f81b9bd85417ba123f92c848a8662
csandrisTurr <csaszarandras@turr.hu>
2024-02-28 11:36:10 +0100

    feat: started user creation

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { DbUser, DbUserName } from 'src/schema/db/User';
+import { RegisterUserDto } from 'src/schema/dto/User';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectModel(DbUserName) private readonly userModel: Model<DbUser>,
+    ) {}
+
+    async createUser(data: RegisterUserDto) {
+        const dbUser = new this.userModel({
+            username: data.username,
+            displayName: data.displayName,
+            creationDate: new Date(),
+            updateDate: new Date(),
+            email: data.email,
+            password: data.password,
+        });
+
+        await dbUser.save();
+    }
+}
