3287508cc480dd3102798f5a9896821d10dc604f
csandrisTurr <csaszarandras@turr.hu>
2024-03-04 08:44:41 +0100

    feat: make auth routes return RestResponse

--- src/user/auth.controller.ts
+++ src/user/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, NotImplementedException, Post } from '@nestjs/common';
+import { RestResponse } from 'src/schema/dto/RestResponse';
 import { LoginUserDto, RegisterUserDto } from 'src/schema/dto/User';
 import { UserService } from './user.service';
 
@@ -7,12 +8,15 @@ export class AuthController {
     constructor (private readonly userService: UserService) {}
 
     @Post('login')
-    async login(@Body() dto: LoginUserDto) {
+    async login(@Body() dto: LoginUserDto): Promise<RestResponse> {
         throw new NotImplementedException();
     }
 
     @Post('register')
-    async register(@Body() dto: RegisterUserDto) {
-        await this.userService.createUser(dto);
+    async register(@Body() dto: RegisterUserDto): Promise<RestResponse> {
+        return RestResponse.ok(
+            await this.userService.createUser(dto),
+            201,
+        );
     }
 }
