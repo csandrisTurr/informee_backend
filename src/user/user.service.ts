3248308c106bd20b6283d0513ccfe3b888c0b707
csandrisTurr <csaszarandras@turr.hu>
2024-03-25 01:44:02 +0100

    feat: refactor and jwt renew in service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,47 +2,61 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { DbUser, DbUserName } from 'src/schema/db/User';
-import { RegisterUserDto } from 'src/schema/dto/User';
+import { JwtPayload, RegisterUserDto } from 'src/schema/dto/User';
 import * as argon2 from 'argon2';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UserService {
-    constructor(
-        @InjectModel(DbUserName) private readonly userModel: Model<DbUser>,
-        private readonly jwtService: JwtService,
-    ) {}
+  constructor(
+    @InjectModel(DbUserName) private readonly userModel: Model<DbUser>,
+    private readonly jwtService: JwtService,
+  ) {}
 
-    async login(email: string, password: string): Promise<any> {
-        const dbUser = await this.userModel.findOne({ email });
+  // @returns the jwt token for the user
+  async login(email: string, password: string): Promise<string> {
+    const dbUser = await this.userModel.findOne({ email });
 
-        if (!dbUser || !await argon2.verify(dbUser.password, password)) {
-            throw new UnauthorizedException();
-        }
-
-        // return jwt user object
-        return {
-            token: await this.jwtService.signAsync({
-                id: dbUser.id,
-                username: dbUser.username,
-            }),
-        }
+    if (!dbUser || !(await argon2.verify(dbUser.password, password))) {
+      throw new UnauthorizedException();
     }
 
-    async renewJwt(jwt: string) {
-        // TODO
+    const jwtPayload: JwtPayload = {
+      id: dbUser.id,
+      username: dbUser.username,
+    };
+
+    // return jwt user object
+    return await this.jwtService.signAsync(jwtPayload);
+  }
+
+  // @returns the renewed jwt
+  async renewJwt(userId: string, password: string): Promise<string> {
+    // no error handling because guard already validated jwt
+    const dbUser = await this.userModel.findOne({ _id: userId });
+
+    if (!dbUser || !(await argon2.verify(dbUser.password, password))) {
+      throw new UnauthorizedException();
     }
 
-    async createUser(data: RegisterUserDto) {
-        const dbUser = new this.userModel({
-            username: data.username,
-            displayName: data.displayName,
-            creationDate: new Date(),
-            updateDate: new Date(),
-            email: data.email,
-            password: await argon2.hash(data.password),
-        });
+    const newJwtPayload = {
+      id: dbUser.id,
+      username: dbUser.username,
+    };
 
-        await dbUser.save();
-    }
+    return await this.jwtService.signAsync(newJwtPayload);
+  }
+
+  async createUser(data: RegisterUserDto) {
+    const dbUser = new this.userModel({
+      username: data.username,
+      displayName: data.displayName,
+      creationDate: new Date(),
+      updateDate: new Date(),
+      email: data.email,
+      password: await argon2.hash(data.password),
+    });
+
+    await dbUser.save();
+  }
 }
