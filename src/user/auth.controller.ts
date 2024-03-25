a690945ffec4c90cafd3f87f5e1196efd2a70b0f
csandrisTurr <csaszarandras@turr.hu>
2024-03-25 01:48:04 +0100

    feat: renewToken route + refactoring

--- src/user/auth.controller.ts
+++ src/user/auth.controller.ts
@@ -1,25 +1,39 @@
-import { Body, Controller, NotImplementedException, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { RestResponse } from 'src/schema/dto/RestResponse';
-import { LoginUserDto, RegisterUserDto } from 'src/schema/dto/User';
+import { JwtPayload, LoginUserDto, RegisterUserDto, RenewTokenDto } from 'src/schema/dto/User';
 import { UserService } from './user.service';
+import { AuthGuard } from './auth.guard';
+import { User } from './user.decorator';
+import { Public } from './public.decorator';
 
 @Controller('auth')
 export class AuthController {
-    constructor (private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) {}
 
-    @Post('login')
-    async login(@Body() dto: LoginUserDto): Promise<RestResponse> {
-        return RestResponse.ok(
-            await this.userService.login(dto.email, dto.password),
-            200,
-        );
-    }
+  @Public()
+  @Post('login')
+  async login(@Body() dto: LoginUserDto): Promise<RestResponse> {
+    return RestResponse.ok(
+      {
+        token: await this.userService.login(dto.email, dto.password),
+      },
+      200,
+    );
+  }
 
-    @Post('register')
-    async register(@Body() dto: RegisterUserDto): Promise<RestResponse> {
-        return RestResponse.ok(
-            await this.userService.createUser(dto),
-            201,
-        );
-    }
+  @Post('renewToken')
+  async renewToken(@Body() dto: RenewTokenDto, @User() user: JwtPayload): Promise<RestResponse> {
+    return RestResponse.ok(
+      {
+        token: await this.userService.renewJwt(user.id, dto.password),
+      },
+      200,
+    );
+  }
+
+  @Public()
+  @Post('register')
+  async register(@Body() dto: RegisterUserDto): Promise<RestResponse> {
+    return RestResponse.ok(await this.userService.createUser(dto), 201);
+  }
 }
