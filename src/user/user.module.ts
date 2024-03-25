cb3da0aa99e2bb46b17275506692bee28edd3fa0
csandrisTurr <csaszarandras@turr.hu>
2024-03-25 01:46:38 +0100

    fix: jwt secret not coming from env + auth guard

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,20 +4,35 @@ import { UserController } from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DbUserName, DbUserSchema } from 'src/schema/db/User';
 import { AuthController } from './auth.controller';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth.guard';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: DbUserName, schema: DbUserSchema }]),
-    JwtModule.register({
-      global: true,
-      secret: 'asdasadsadssaddsa',
-      signOptions: {
-        expiresIn: '1d',
-      }
-    })
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => {
+        return {
+          global: true,
+          signOptions: {
+            expiresIn: '1d',
+          },
+          secret: configService.get<string>('JWT_SECRET'),
+        };
+      },
+      inject: [ConfigService],
+    }),
   ],
-  providers: [UserService],
+  providers: [
+    UserService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   controllers: [UserController, AuthController],
+  exports: [JwtModule],
 })
 export class UserModule {}
