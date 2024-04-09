b3fc5f2f8b9ed7ee8aabfd9dd09f284276557c41
csandrisTurr <csaszarandras@turr.hu>
2024-04-09 08:45:43 +0200

    feat: post CRUD route defs

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, NotImplementedException, Patch, Post } from '@nestjs/common';
 
 @Controller('post')
-export class PostController {}
+export class PostController {
+    @Get(':id')
+    async getPost() {
+        throw new NotImplementedException();
+    }
+
+    @Post()
+    async createPost() {
+        throw new NotImplementedException();
+    }
+
+    @Patch(':id')
+    async updatePost() {
+        throw new NotImplementedException();
+    }
+
+    @Delete(':id')
+    async deletePost() {
+        throw new NotImplementedException();
+    }
+}
