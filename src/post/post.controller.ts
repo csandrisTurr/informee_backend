bb9a5d3d7cff3ce5ed84395b33e0f6310a77d7c3
csandrisTurr <csaszarandras@turr.hu>
2024-04-24 11:19:31 +0200

    ref: post controller format

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -7,50 +7,40 @@ import { PostService } from './post.service';
 
 @Controller('post')
 export class PostController {
-    constructor (private readonly postService: PostService) {}
+  constructor(private readonly postService: PostService) {}
 
-    @Get(':id?')
-    async getPost(@Param('id') postId: string, @User() user: JwtPayload) {
-        return RestResponse.ok(
-            await this.postService.getPosts(user.id, postId),
-            200,
-        );
-    }
+  @Get(':id?')
+  async getPost(@Param('id') postId: string, @User() user: JwtPayload) {
+    return RestResponse.ok(await this.postService.getPosts(user.id, postId), 200);
+  }
 
-    @Post()
-    async createPost(@Body() data: CreatePostDto, @User() user: JwtPayload) {
-        return RestResponse.ok(
-            await this.postService.createPost(data, user.id),
-            200,
-        );
-    }
-    
-    @Patch(':id/set_content')
-    async setContent(@Body() data: PostSetContentDto, @Param('id') postId: string, @User() user: JwtPayload) {
-        return RestResponse.ok(
-            await this.postService.setContent(postId, user.id, data.content),
-            200,
-        );
-    }
+  @Post()
+  async createPost(@Body() data: CreatePostDto, @User() user: JwtPayload) {
+    return RestResponse.ok(await this.postService.createPost(data, user.id), 200);
+  }
 
-    @Patch(':id/set_private/:bool')
-    async setPrivate(@Body() data: PostSetContentDto, @Param('id') postId: string, @Param('bool') bool: string, @User() user: JwtPayload) {
-        return RestResponse.ok(
-            await this.postService.setPrivate(postId, user.id, bool == '1' ? true : false),
-            200,
-        );
-    }
+  @Patch(':id/set_content')
+  async setContent(@Body() data: PostSetContentDto, @Param('id') postId: string, @User() user: JwtPayload) {
+    return RestResponse.ok(await this.postService.setContent(postId, user.id, data.content), 200);
+  }
 
-    @Patch(':id')
-    async updatePost() {
-        throw new NotImplementedException();
-    }
+  @Patch(':id/set_private/:bool')
+  async setPrivate(
+    @Body() data: PostSetContentDto,
+    @Param('id') postId: string,
+    @Param('bool') bool: string,
+    @User() user: JwtPayload,
+  ) {
+    return RestResponse.ok(await this.postService.setPrivate(postId, user.id, bool == '1' ? true : false), 200);
+  }
 
-    @Delete(':id')
-    async deletePost(@Param('id') postId: string, @User() user: JwtPayload) {
-        return RestResponse.ok(
-            await this.postService.deletePost(postId, user.id),
-            200,
-        );
-    }
+  @Patch(':id')
+  async updatePost() {
+    throw new NotImplementedException();
+  }
+
+  @Delete(':id')
+  async deletePost(@Param('id') postId: string, @User() user: JwtPayload) {
+    return RestResponse.ok(await this.postService.deletePost(postId, user.id), 200);
+  }
 }
