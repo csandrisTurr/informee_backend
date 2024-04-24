86ca136a698b775430225c06db9e2ed7f1a5fd2e
csandrisTurr <csaszarandras@turr.hu>
2024-04-24 11:19:01 +0200

    fix: post schema not injecting + formatting

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DbPostName, DbPostSchema } from 'src/schema/db/Post';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: DbPostName, schema: DbPostSchema }])],
   providers: [PostService],
-  controllers: [PostController]
+  controllers: [PostController],
 })
 export class PostModule {}
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,72 +1,61 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { DbPostDocument } from 'src/schema/db/Post';
+import { DbPostDocument, DbPostName } from 'src/schema/db/Post';
 import { CreatePostDto } from 'src/schema/dto/Post';
 import { RestResponse } from 'src/schema/dto/RestResponse';
 
 @Injectable()
 export class PostService {
-    constructor (private readonly postModel: Model<DbPostDocument>) {}
+  constructor(@InjectModel(DbPostName) private readonly postModel: Model<DbPostDocument>) {}
 
-    async getPosts(userId: string, idFilter?: string): Promise<DbPostDocument[]> {
-        return await this.postModel.find({ authorId: userId, id: idFilter });
-    }
+  async getPosts(userId: string, idFilter?: string): Promise<DbPostDocument[]> {
+    return await this.postModel.find({ authorId: userId, id: idFilter });
+  }
 
-    async createPost(data: CreatePostDto, userId: string): Promise<DbPostDocument> {
-        const dbPost = new this.postModel({
-            authorId: userId,
-            title: data.title,
-            tags: [],
-            private: data.private,
-            description: data.description,
-            updateDate: new Date(),
-            creationDate: new Date(),
-        });
+  async createPost(data: CreatePostDto, userId: string): Promise<DbPostDocument> {
+    const dbPost = new this.postModel({
+      authorId: userId,
+      title: data.title,
+      tags: [],
+      private: data.private,
+      description: data.description,
+      updateDate: new Date(),
+      creationDate: new Date(),
+    });
 
-        await dbPost.save();
+    await dbPost.save();
 
-        return dbPost;
-    }
+    return dbPost;
+  }
 
-    async setContent(postId: string, userId: string, newContent: string): Promise<boolean> {
-        const dbPost = await this.postModel.findOne({ id: postId, authorId: userId })
+  async setContent(postId: string, userId: string, newContent: string): Promise<boolean> {
+    const dbPost = await this.postModel.findOne({ id: postId, authorId: userId });
 
-        if (dbPost.authorId != userId)
-            throw RestResponse.err(
-                400,
-                'NO'
-            );
+    if (dbPost.authorId != userId) throw RestResponse.err(400, 'NO');
 
-        dbPost.content = newContent;
-        await dbPost.save();
+    dbPost.content = newContent;
+    await dbPost.save();
 
-        return true;
-    }
+    return true;
+  }
 
-    async setPrivate(postId: string, userId: string, bool: boolean): Promise<boolean> {
-        const dbPost = await this.postModel.findOne({ id: postId, authorId: userId })
+  async setPrivate(postId: string, userId: string, bool: boolean): Promise<boolean> {
+    const dbPost = await this.postModel.findOne({ id: postId, authorId: userId });
 
-        if (dbPost.authorId != userId)
-            throw RestResponse.err(
-                400,
-                'NO'
-            );
+    if (dbPost.authorId != userId) throw RestResponse.err(400, 'NO');
 
-        dbPost.private = bool;
-        await dbPost.save();
+    dbPost.private = bool;
+    await dbPost.save();
 
-        return true;
-    }
+    return true;
+  }
 
-    async deletePost(postId: string, userId: string): Promise<boolean> {
-        const dbPost = await this.postModel.findOne({ id: postId, authorId: userId })
+  async deletePost(postId: string, userId: string): Promise<boolean> {
+    const dbPost = await this.postModel.findOne({ id: postId, authorId: userId });
 
-        if (!dbPost)
-            throw RestResponse.err(
-                404,
-                'NOT_FOUND',
-            );
+    if (!dbPost) throw RestResponse.err(404, 'NOT_FOUND');
 
-        return true;
-    }
+    return true;
+  }
 }
