0db19125b4e2b65878d676d277d1a91d0cbdd573
csandrisTurr <csaszarandras@turr.hu>
2024-05-13 11:31:56 +0200

    feat: review controller methods

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, NotImplementedException, Post } from '@nestjs/common';
 
 @Controller('review')
-export class ReviewController {}
+export class ReviewController {
+  @Post()
+  async createReview() {
+    throw new NotImplementedException();
+  }
+
+  @Post(':id')
+  async editReview() {
+    throw new NotImplementedException();
+  }
+  
+  @Post(':id')
+  async deleteReview() {
+    throw new NotImplementedException();
+  }
+}
