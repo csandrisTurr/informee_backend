75538f09f1d37b18ad29b1229b39c01e8fd6c4c2
csandrisTurr <csaszarandras@turr.hu>
2024-05-27 00:36:23 +0200

    fix: registration regex not working

--- src/schema/dto/User.ts
+++ src/schema/dto/User.ts
@@ -6,11 +6,11 @@ export class RegisterUserDto {
   email: string;
 
   @IsString()
-  @Matches(/^[a-z0-9_-]{3,24}$/)
+  @Matches('[a-z0-9_-]{3,24}')
   username: string;
 
   @IsString()
-  @Matches(/^[a-zA-Z0-9 ]{3,24}$/)
+  @Matches('[a-zA-Z0-9 ]{3,24}')
   displayName: string;
 
   // TODO: add check for sha512
